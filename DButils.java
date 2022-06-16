08398d0f5768204ed394b3c4024857255169d3fa
khitish1993 <khitishkumar100293@gmail.com>
2022-06-16 21:05:46 +0530

    updated

--- DButils.java
+++ DButils.java
@@ -1,5 +1,10 @@
 Hello this line is updated in dev branch...
 hello this line is updated in dev branch.
+<<<<<<< HEAD
 Hello Guys..............
 somu
+=======
+Hello Guys..
+
+>>>>>>> 52897bd (updated)
 db logic added..
